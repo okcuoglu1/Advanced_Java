2e20bfb8694dbf83280bdf1d386fe34e47c20e6e
okcuoglu1 <okcuoglucemil@gmail.com>
2023-03-11 21:43:57 +0300

    Bounding

--- src/Bounding/GenericUpperBound.java
+++ src/Bounding/GenericUpperBound.java
@@ -1,4 +1,48 @@
 package Bounding;
+//parametre olarak aldığımız data tipini sınırlandırabiliriz.
+//bu classta sadece Number ın alt sınıfları kullanılsın
+public class GenericUpperBound<T extends Number> {
+    //T:Integer,Double,Long,Float,Number; classı üstten sınırlandırdık
+    //T upperbound olarak Number class'ini extend ettiginden Number Class'inin methodlarini kullanabiliyor.
 
-public class GenericUpperBound {
+    private T[] numberArray;//field,özellik
+
+    public GenericUpperBound(T[] numberArray) {
+        this.numberArray = numberArray;
+    }
+    //numberArray içindeki değerlerin ortalamsını bulan metod
+    public double getAvarage(){
+        double sum=0;
+        for (T t:this.numberArray) {
+            sum+=t.doubleValue();
+        }
+
+        double avg=sum/this.numberArray.length;
+        return avg;
+
+    }
+
+    //main-metod
+    public static void main(String[] args) {
+        Integer[] intArr={2,3,5,99};
+        Double[] doubleArr={2.3,5.5,88.9};
+        String[] strArr={"Generic","tipleri","üstten","sınırlandırabiliriz"};
+
+
+        GenericUpperBound<Integer> obj1=new GenericUpperBound<>(intArr);
+        System.out.println(obj1.getAvarage());
+
+        GenericUpperBound<Double> obj2=new GenericUpperBound<>(doubleArr);
+        System.out.println(obj2.getAvarage());
+
+        //   GenericUpperBound<String> obj3=new GenericUpperBound<>(strArr);-->CTE data tipini number ile sınırlandırdık.
+
+
+    }
+
 }
+
+
+
+
+
